ba31fc907e45e3913c31e6da101241089904eb7a
Diogo Machado <diogomachado_8@hotmail.com>
2020-11-22 14:41:18 -0300

    console port

--- src/server.ts
+++ src/server.ts
@@ -2,5 +2,5 @@ import app from './app';
 
 app.server.listen(process.env.PORT || 3333, () => {
   // eslint-disable-next-line no-console
-  console.log('Running...');
+  console.log(`Running... ${process.env.PORT}` || 3333);
 });
